a0cd3ea02de8f3a31ca51c0f78bf41c88c69743b
chrislee1996 <prepchrislee@gmail.com>
2022-08-03 15:14:43 -0400

    api rendering

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,21 +1,33 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import env from 'react-dotenv'
 
 const Popular = () => {
+    const [popular, setPopular] = useState([])
 
-useEffect(()=> {
-    getPopular()
-},[])
-    
-const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-    const data = await api.json()
-    console.log(data,'here is our data')
-}
+    useEffect(()=> {
+        getPopular()
+    },[])
+        
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+        const data = await api.json()
+        // console.log(data,'here is our data')
+        setPopular(data.recipes) 
+    }
 
 
   return (
-    <div>Popular</div>
+    <div>
+        Popular
+        {popular.map((recipe) => {
+            return (
+                <div>
+                    <p>{recipe.title}</p>
+                </div>
+            )
+        })}
+
+    </div>
   )
 }
 
