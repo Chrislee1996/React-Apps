1d08d0b1771d2b669c968fdbb16916d96fce3834
chrislee1996 <prepchrislee@gmail.com>
2022-08-04 14:13:32 -0400

    categories now showing

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,28 +7,28 @@ import {NavLink} from 'react-router-dom'
 const Category = () => {
   return (
     <List>
-        <NavLink to ={'/cuisine/Italian'}> 
+        <SLink to ={'/cuisine/Italian'}> 
         <FaPizzaSlice/> 
         <h3> Italian </h3>
-        </NavLink>
+        </SLink>
 
 
-        <NavLink to ={'/cuisine/American'}> 
+        <SLink to ={'/cuisine/American'}> 
         <FaHamburger/>
         <h3> American </h3>
-        </NavLink>
+        </SLink>
 
 
-        <NavLink to ={'/cuisine/Thai'}> 
+        <SLink to ={'/cuisine/Thai'}> 
         <GiNoodles/> 
         <h3> Thai </h3>
-        </NavLink>
+        </SLink>
 
 
-        <NavLink to ={'/cuisine/Chinese'}> 
+        <SLink to ={'/cuisine/Chinese'}> 
         <GiChopsticks/> 
         <h3> Chinese </h3>
-        </NavLink>
+        </SLink>
 
     </List>
   )
@@ -38,6 +38,44 @@ const List = styled.div`
     display:flex;
     justify-content:center;
     margin:2rem 0rem;
-`
+`;
+
+const SLink = styled(NavLink)`
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+border-radius:50%;
+margin-right:2rem;
+text-decoration:none;
+background:linear-gradient(35deg,#494949, #313131);
+width:6rem;
+height:6rem;
+cursor:pointer;
+transform:scale(0.8);
+
+h4 {
+  color:white;
+  font-size:0.8rem;
+}
+
+svg{
+  color:white;
+  font-size:1.5rem;
+}
+
+&.active{
+  background:linear-gradient(to right, #f27121, #e94057)
+
+  svg {
+    color:white;
+  }
+
+  h4 {
+    color:white;
+  }
+}
+`;
+
 
 export default Category
