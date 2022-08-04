fdf6e67857b9d19fae4aed3a8f0ebe47c3339258
chrislee1996 <prepchrislee@gmail.com>
2022-08-04 14:59:28 -0400

    search function works

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,5 +1,6 @@
-import {useEffect, useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
+import styled from 'styled-components'
 
 const Searched = () => {
     const [searchedRecipes, setSearchedRecipes] = useState([])
@@ -9,16 +10,46 @@ const Searched = () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`)
         const recipes = await data.json()
         setSearchedRecipes(recipes.results)
+        console.log(recipes.results,'results searched')
       }
 
 
       useEffect(() => {
-        getSearched(params.searched)
-      },[])
+        getSearched(params.search)
+      },[params.search])
 
   return (
-    <div>Searched</div>
+    <Grid>
+        {searchedRecipes.map((item) =>{
+            return(
+                <Card key ={item.id}>
+                    <img src={item.image} alt=''/>
+                    <h4>{item.title}</h4>
+                </Card>
+            )
+        })}
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+display:grid;
+grid-template-columns: repeat(auto-fit, minmax(20rem,1fr));
+grid-grap:3rem;
+`
+const Card= styled.div`
+  img{
+    width:100%;
+    border-radius:2rem;
+  }
+  a {
+    text-decoration:none;
+  }
+  h4{
+    text-align: center;
+    padding:1rem;
+  }
+`
+
 
 export default Searched
