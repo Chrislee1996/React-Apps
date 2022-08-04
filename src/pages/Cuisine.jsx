e7afb8119b11ffa912d97f35ec968b41e5de4250
chrislee1996 <prepchrislee@gmail.com>
2022-08-04 15:24:05 -0400

    added pages for recipes

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -20,6 +20,7 @@ const Cuisine = () => {
     const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
     const recipes = await data.json()
     setCuisine(recipes.results)
+    console.log(recipes.results,'recipes result')
   }
 
 
